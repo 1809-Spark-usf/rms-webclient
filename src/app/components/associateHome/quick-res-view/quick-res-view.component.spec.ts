c0de81bd78d7812db8ce398dc4333b3987f31854
Jose Meono <jose.a.meono@gmail.com>
2019-03-21 14:05:31 -0400

    implemented testing for quick-res

--- src/app/components/associateHome/quick-res-view/quick-res-view.component.spec.ts
+++ src/app/components/associateHome/quick-res-view/quick-res-view.component.spec.ts
@@ -3,35 +3,51 @@ import { async, ComponentFixture, inject, TestBed } from '@angular/core/testing'
 import { QuickResViewComponent } from './quick-res-view.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ReservationService } from 'src/app/services/reservation/reservation.service';
+import { FormBuilder } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Reservation } from 'src/app/models/reservation';
+import { Subject, Observable } from 'rxjs';
 
 describe('QuickResViewComponent', () => {
   let component: QuickResViewComponent;
   let fixture: ComponentFixture<QuickResViewComponent>;
-  let testBedService: ReservationService;
+  let reservationServiceStub: Partial<ReservationService>;
+  let reservationService;
 
   beforeEach(async(() => {
+    reservationServiceStub = {
+      currentReservation: new Reservation(),
+      $currentReservation: new Subject<Reservation>(),
+      userReservations: [new Reservation(), new Reservation()],
+      $userReservations: new Subject<Reservation[]>(),
+    };
     TestBed.configureTestingModule({
+      providers: [{ provide: ReservationService, userValue: reservationServiceStub }],
       declarations: [QuickResViewComponent],
-      providers: [ReservationService],
-      imports: [HttpClientModule],
+      imports: [HttpClientTestingModule],
     })
       .compileComponents();
   }));
 
-  beforeEach(async () => {
+  beforeEach(() => {
     fixture = TestBed.createComponent(QuickResViewComponent);
+    reservationService = fixture.debugElement.injector.get(ReservationService);
     component = fixture.componentInstance;
+    reservationService = TestBed.get(ReservationService);
     fixture.detectChanges();
-    testBedService = TestBed.get(ReservationService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('Service injected and testBed Service are same instance',
-    inject([ReservationService], (injectedService: ReservationService) => {
-      expect(injectedService).toBe(testBedService);
-    }),
-  );
+  describe('viewReservations', () => {
+    it('should hide results', () => {
+      expect(component.loaded).toBeFalsy();
+    });
+    // it('updates list', () => {
+    //   let dummyReservationList: Reservation[] = [new Reservation(), new Reservation()];
+    // });
+  });
+
 });
