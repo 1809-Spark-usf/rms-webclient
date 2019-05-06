e5666fdaaede99084dd6f333e7f5c5e31fbc892f
QuintonCook <Quintoncook97@hotmail.com>
2019-05-06 13:48:52 -0500

    fixed the it should create test

--- src/app/components/reservations/update-reservation-popup/update-reservation-popup.component.spec.ts
+++ src/app/components/reservations/update-reservation-popup/update-reservation-popup.component.spec.ts
@@ -3,6 +3,20 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { UpdateReservationPopupComponent } from './update-reservation-popup.component';
 
 describe('UpdateReservationPopupComponent', () => {
+  let ngbModalStub: {dismiss: jasmine.Spy};
+  let resourceServiceStub: {
+    getAvailableResources: jasmine.Spy,
+    pushNewCurrentResourceList: jasmine.Spy
+  }
+  let userServiceStub = { currentUser: undefined };
+  let reservationServiceStub = {
+      cancelReservations: undefined,
+      getUserReservations: undefined,
+      pushNewUserReservations: undefined
+  }
+  
+  let routerStub: {navigate: jasmine.Spy};
+
   let component: UpdateReservationPopupComponent;
   let fixture: ComponentFixture<UpdateReservationPopupComponent>;
 
@@ -14,12 +28,21 @@ describe('UpdateReservationPopupComponent', () => {
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(UpdateReservationPopupComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    userServiceStub = { currentUser: undefined };
+    ngbModalStub = { dismiss: undefined };
+    reservationServiceStub = {
+        cancelReservations: undefined,
+        getUserReservations: undefined,
+        pushNewUserReservations: undefined
+    }
+    resourceServiceStub = {
+      getAvailableResources: undefined,
+      pushNewCurrentResourceList: undefined
+    }
   });
 
   it('should create', () => {
+    component = new UpdateReservationPopupComponent(<any>ngbModalStub, <any>reservationServiceStub, <any>userServiceStub, <any>resourceServiceStub, <any> routerStub);
     expect(component).toBeTruthy();
-  });
+});
 });
